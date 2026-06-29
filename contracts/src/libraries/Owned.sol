0afc8237c1a018fdb8ac1182cac047bb4c56459e
pipeshiftprotocol <12866635+pipeshiftprotocol@users.noreply.github.com>
2026-06-29 13:41:00 +0000

    refactor(contracts): two step ownership transfer

--- contracts/src/libraries/Owned.sol
+++ contracts/src/libraries/Owned.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.24;
 
 abstract contract Owned {
     address public owner;
+    address public pendingOwner;
 
+    event OwnershipTransferStarted(address indexed from, address indexed to);
     event OwnershipTransferred(address indexed from, address indexed to);
 
     error NotOwner(address caller);
+    error NotPendingOwner(address caller);
     error ZeroOwner();
 
     modifier onlyOwner() {
@@ -22,9 +25,16 @@ abstract contract Owned {
 
     function transferOwnership(address to) external onlyOwner {
         if (to == address(0)) revert ZeroOwner();
+        pendingOwner = to;
+        emit OwnershipTransferStarted(msg.sender, to);
+    }
+
+    function acceptOwnership() external {
+        if (msg.sender != pendingOwner) revert NotPendingOwner(msg.sender);
 
         address previous = owner;
-        owner = to;
-        emit OwnershipTransferred(previous, to);
+        owner = pendingOwner;
+        pendingOwner = address(0);
+        emit OwnershipTransferred(previous, owner);
     }
 }
